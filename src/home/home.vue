270e9f1ef56de67f8161527e2f293c5fb729ef2e
thiago-figueiredo <tfigueiredo@gmail.com>
2021-04-15 15:10:49 -0300

    Improved home screen structure.

--- src/home/home.vue
+++ src/home/home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="container">GB</div>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm">
+        <h1>GB</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
